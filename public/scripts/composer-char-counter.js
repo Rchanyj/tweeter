367d4f14de0adf1627a449570c6f735cb6d4eac3
Raionos <rchan1992@gmail.com>
2017-10-18 00:27:04 +0000

    working char counter, turns red when below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,11 +7,17 @@ $(document).ready(function () {
 
 //Register event handler for textarea element of form inside of .new-tweet
 // Using this to grab the VALUE of the textarea, determine length of input
+// update the value of the counter to this value
 
     $(".new-tweet textarea").on("keyup", function() {
       var chars = $(this).val().length;
       var charsleft = 140 - chars;
-      $(this).parent("form").find(".counter")[0].innerText = charsleft;
-    });
+      var counter = $(this).parent("form").find(".counter");
+      counter[0].innerText = charsleft;
 
+    //Turn counter number red if below 0:
+      if (charsleft < 0) {
+        counter.css("color", "red");
+      }
+    });
 });
