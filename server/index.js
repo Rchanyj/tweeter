92e0278b3c5fee6b1b207fb61fb865a6bd47e64f
Raionos <rchan1992@gmail.com>
2017-10-19 22:49:00 +0000

    moved tweets storage to MongoDB

--- server/index.js
+++ server/index.js
@@ -6,29 +6,43 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const {MongoClient} = require("mongodb");
+const TWEETHOST     = "mongodb://localhost:27017/tweets";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// Change this to connect to MongoDB instead:
 
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+MongoClient.connect(TWEETHOST, (err, db) => {
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  if (err) {
+    console.error(`Failed to connect: ${TWEETHOST}`);
+    throw err;
+  }
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+  console.log(`Successfully connected to mongodb: ${TWEETHOST}`);
 
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  // The `data-helpers` module provides an interface to the database of tweets.
+  // This simple interface layer has a big benefit: we could switch out the
+  // actual database it uses and see little to no changes elsewhere in the code
+  // (hint hint).
+  //
+  // Because it exports a function that expects the `db` as a parameter, we can
+  // require it and pass the `db` parameter immediately:
+  // Once updated and connected to Mongo, pass the Mongo db to this instead:
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+
+  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+  // so it can define routes that use it to interact with the data layer.
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
+  // Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
+
+  app.listen(PORT, () => {
+    console.log("Example app listening on port " + PORT);
+  });
+
 });
